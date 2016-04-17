76c9e4ae43870d4417372156866a5c9d5024bb80
JesterJeffrey <epicjesta@gmail.com>
2016-04-16 19:22:43 -0700

    Ugh

--- js/custom.js
+++ js/custom.js
@@ -109,25 +109,26 @@ jQuery(document).ready(function(){
 		$success		= 'Your message has been sent. Thank you!';
 		
 		
-		$contactForm.submit(function(e) {
-			e.preventDefault();
-			$.ajax({
-				url: '//formspree.io/jeffrey@preparedfor.work',
-				method: 'POST',
-				data: $(this).serialize(),
-				dataType: 'json',
-				success: function(data) {
-					response = '<div class="success">'+ $success +'</div>';
-				},
-				error: function(err) {
-					response = '<div class="error">There was an issue submitting your message. Please try again.</div>';
-				}
-			});
-			// Hide any previous response text
-			$(".error,.success").remove();
-			// Show response message
-			$contactForm.prepend(response);
-		});
+$contactForm.submit(function(e) {
+	e.preventDefault();
+	$.ajax({
+		url: '//formspree.io/your@email.com',
+		method: 'POST',
+		data: $(this).serialize(),
+		dataType: 'json',
+		beforeSend: function() {
+			$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+		},
+		success: function(data) {
+			$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+		},
+		error: function(err) {
+			$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+		}
+	});
+});
 		
 	/* ---------------------------------------------------------------------- */
 	/*	Google Maps
