bf0dbedfcbf0944ca52d6696f26aa1c9ac53218e
JesterJeffrey <epicjesta@gmail.com>
2016-04-16 19:33:13 -0700

    form take 500

--- js/custom.js
+++ js/custom.js
@@ -105,31 +105,17 @@ jQuery(document).ready(function(){
 	/* ---------------------------------------------------------------------- */
 	
 	// Needed variables
-	var $contactForm 	= $('#contactform'),
+	var $contactform 	= $('#contactform'),
 		$success		= 'Your message has been sent. Thank you!';
 		
-		
-$contactForm.submit(function(e) {
-	e.preventDefault();
-	$.ajax({
-		url: '//formspree.io/your@email.com',
-		method: 'POST',
-		data: $(this).serialize(),
-		dataType: 'json',
-		beforeSend: function() {
-			$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
-		},
-		success: function(data) {
-			$contactForm.find('.alert--loading').hide();
-			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
-		},
-		error: function(err) {
-			$contactForm.find('.alert--loading').hide();
-			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
-		}
-	});
-});
-		
+	$contactform.submit(function(){
+		response = '<div class="success">'+ $success +'</div>'
+		// Hide any previous response text
+		$(".success").remove();
+		// Show response message
+		$contactform.prepend(response);
+		return false;
+	});	
 	/* ---------------------------------------------------------------------- */
 	/*	Google Maps
 	/* ---------------------------------------------------------------------- */
